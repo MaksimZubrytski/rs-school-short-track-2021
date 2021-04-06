a662632385827428d23eccf98a84993ff26b029a
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-04-06 16:10:55 +0300

    feat: add sovled common character count task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,20 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  let newString2 = s2;
+  const min = Math.min(s1.length, s2.length);
+  for (let i = 0; i < min; i++) {
+    if (newString2.includes(s1[i])) {
+      newString2 = newString2.replace(s1[i], '');
+      count += 1;
+    }
+  }
+
+  return count;
 }
+
+getCommonCharacterCount('aabcc', 'ascaa');
 
 module.exports = getCommonCharacterCount;
