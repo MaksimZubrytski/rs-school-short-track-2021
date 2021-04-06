047408ac4c9a5800fb31d3890a6fbb7cebac7382
MaksimZubrytski <maksimxdd@gmail.com>
2021-04-06 13:36:47 +0300

    feat: add solved maxtrix-elements task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,21 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let accSum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        accSum += matrix[i][j];
+      } else if (matrix[i - 1][j] === 0) {
+        accSum += 0;
+      } else {
+        accSum += matrix[i][j];
+      }
+    }
+  }
+
+  return accSum;
 }
 
 module.exports = getMatrixElementsSum;
